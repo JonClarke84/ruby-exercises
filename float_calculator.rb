bb3154e656ca8960efce2d262d0ca0ad26f321a2
Jon Clarke <jonclarke1984@googlemail.com>
2022-02-23 12:40:40 +0000

    Added float calculator

---
+++ float_calculator.rb
@@ -0,0 +1,9 @@
+#ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+puts "Enter first number: "
+first_num = gets.chomp
+
+puts "Enter second number: "
+second_num = gets.chomp
+
+puts "#{first_num} divided by #{second_num.to_i} is #{first_num.to_f/second_num.to_f}."
