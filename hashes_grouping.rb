1640a35567e699f6a398913343b557e229d86eb2
Jon Clarke <jonclarke1984@googlemail.com>
2022-02-24 16:07:42 +0000

    Hash group update

--- hashes_grouping.rb
+++ hashes_grouping.rb
@@ -7,16 +7,6 @@ players = [
 
   sorted_by_sport = {}
 
-# extract each sport and check if it exists as a key
-
-# if it does not we add it as a key to the new hash
-
-# value is the array of all matching names
-
-# players.each { |player_under_consideration| player_under_consideration.each{ |key, value| (p value) } }
-
-# sorted_by_sport = {}
-
 players.each do |player|
     if !sorted_by_sport.include?(player[:sport])
         sorted_by_sport[player[:sport]] = [player[:name]]
